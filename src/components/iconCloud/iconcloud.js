23e39addc0d933fc1032bfba71f43eafc12dfcd7
Owen R <owenlrichards2000@gmail.com>
2022-08-13 22:02:48 -0400

    8.13.2022 update

---
+++ src/components/iconCloud/iconcloud.js
@@ -0,0 +1,27 @@
+import "./styles.css";
+import { Cloud } from "react-icon-cloud";
+import styled from "styled-components";
+import tools from "./tools";
+
+export const Img = styled.img`
+  object-fit: contain;
+`;
+
+export const A = styled.a`
+  display: flex;
+  flex-direction: column;
+`;
+
+export default function IconCloud() {
+  return (
+    <div className="App">
+      <Cloud>
+        {tools.map((item) => (
+          <A title={item.name}>
+            <Img src={item.icon} width="64" height="64" />
+          </A>
+        ))}
+      </Cloud>
+    </div>
+  );
+}
