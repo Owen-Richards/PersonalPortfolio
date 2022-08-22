9ebd996fdc47f50148e48669cc4a22f7c83535ad
Owen R <owenlrichards2000@gmail.com>
2022-08-21 20:33:20 -0400

    reducing load on CPU

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,12 @@ import Loader from "./components/Loader/loader";
 import Background from "./components/Background/Background";
 import CustomCursor from "./CustomCursor/index";
 import "./styles/home.scss";
+import About from './components/About/About';
+import Skills from './components/Skills/Skills';
+import Project from './components/Projects/Project';
+import Accomplishments from './components/Accomplishments/accomplishments';
+import Experience from './components/Experience/Experience';
+import Contact from './components/Contact/Contact';
 
 const App = () => {
   const [loaded, setLoaded] = useState(false);
@@ -27,6 +33,48 @@ const App = () => {
           data-scroll-container
         >
           <Background/>
+          <br></br>
+      <br></br>
+      <br></br>
+      <br></br>
+      <section id="about">
+        <About />
+      </section>
+      <br></br>
+      <br></br>
+      <br></br>
+      <br></br>
+      <section id="skill">
+        <Skills />
+      </section>
+      <br></br>
+      <br></br>
+      <br></br>
+      <br></br>
+      <section id="project">
+        <Project />
+      </section>
+      <br></br>
+      <br></br>
+      <br></br>
+      <br></br>
+      <section id="experience">
+        <Experience />
+      </section>
+      <br></br>
+      <br></br>
+      <br></br>
+      <br></br>
+      <section id="achievements">
+        <Accomplishments />
+      </section>
+      <br></br>
+      <br></br>
+      <br></br>
+      <br></br>
+      <section id="contact">
+        <Contact />
+      </section>
         </div>
       )}
     </>
